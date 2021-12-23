bffcccb2119d7284b4909249e8ce7d922a5a840f
shahazad08 <sk.shahazad@gmail.com>
2021-12-23 10:03:37 +0530

    Added: Daily Wage Employee Functionality Added

--- src/com/empwage/EmployeeWageBuilder.java
+++ src/com/empwage/EmployeeWageBuilder.java
@@ -5,14 +5,22 @@ import java.util.Random;
 public class EmployeeWageBuilder {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Builder");
-        int IS_PRESENT=1;
+        int IS_FULL_TIME=1;
+        int EMP_RATE_PER_HOUR=20;
+        int empHours=0;
+        int empWage=0;
         Random random = new Random();
         int empCheck= random.nextInt(2);
-        if(empCheck== IS_PRESENT) {
+        if(empCheck== IS_FULL_TIME) {
+            empHours=8;
             System.out.println("Employee is Presnet");
         }
         else {
             System.out.println("Employee is Absent");
+            empHours=0;
         }
+        empWage=empHours*EMP_RATE_PER_HOUR;
+        System.out.println("The daily wage of an employee is" +empWage);
+
     }
 }
