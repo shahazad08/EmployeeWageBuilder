db6a5a908366bf020d537a84cde1e9141ae1b43c
shahazad08 <sk.shahazad@gmail.com>
2021-12-24 09:06:51 +0530

    Added: Part Time Functionality added

--- src/com/empwage/EmployeeWageBuilder.java
+++ src/com/empwage/EmployeeWageBuilder.java
@@ -6,14 +6,19 @@ public class EmployeeWageBuilder {
     public static void main(String args[]) {
         System.out.println("Welcome to Employee Wage Builder");
         int IS_FULL_TIME=1;
+        int IS_PART_TIME=2;
         int EMP_RATE_PER_HOUR=20;
         int empHours=0;
         int empWage=0;
         Random random = new Random();
-        int empCheck= random.nextInt(2);
+        int empCheck= random.nextInt(3);
         if(empCheck== IS_FULL_TIME) {
             empHours=8;
             System.out.println("Employee is Presnet");
+        }
+        else if(empCheck==IS_PART_TIME) {
+            empHours=4;
+            System.out.println("Part time Employee");
         }
         else {
             System.out.println("Employee is Absent");
